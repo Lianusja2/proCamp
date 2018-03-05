a4eeedb973d9dceecd5e5cfed3b829c861a08473
ipshenyc <inna_pshenychna_tp@bmc.com>
2018-03-05 22:31:24 +0200

    add new test for modified addFlower methode

--- FlowerGarden/src/main/java/com/flowergarden/bouquet/MarriedBouquet.java
+++ FlowerGarden/src/main/java/com/flowergarden/bouquet/MarriedBouquet.java
@@ -5,12 +5,18 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import com.flowergarden.exception.IlligalParameterException;
 import com.flowergarden.flowers.GeneralFlower;
+import com.flowergarden.properties.FreshnessInteger;
 
 public class MarriedBouquet implements Bouquet<GeneralFlower> {
 
     private float assemblePrice = 120;
     private List<GeneralFlower> flowerList = new ArrayList<>();
+
+    void setFlowerListForMock(List<GeneralFlower> list) {
+        flowerList = list;
+    }
 
     @Override
     public float getPrice() {
@@ -23,11 +29,26 @@ public class MarriedBouquet implements Bouquet<GeneralFlower> {
 
     @Override
     public void addFlower(GeneralFlower flower) {
+        if (flower.getPrice() < 0) {
+            throw new IlligalParameterException("Can not add flower to bouquet with negative price");
+        }
+        if (flower.getLenght() < 0) {
+            throw new IlligalParameterException("Can not add flower to bouquet with negative length");
+        }
+        if (flower.getFreshness().compareTo(new FreshnessInteger(0)) == -1) {
+            throw new IlligalParameterException("Can not add flower to bouquet with negative freshness");
+        }
         flowerList.add(flower);
     }
 
     @Override
     public Collection<GeneralFlower> searchFlowersByLenght(int start, int end) {
+        if (start > end) {
+            throw new IlligalParameterException(String.format("Min length '%s', can not be greater then max length '%s'", start, end));
+        }
+        if (start < 0) {
+            throw new IlligalParameterException(String.format("Min length '%s', can not be negative", start));
+        }
         List<GeneralFlower> searchResult = new ArrayList<GeneralFlower>();
         for (GeneralFlower flower : flowerList) {
             if (flower.getLenght() >= start && flower.getLenght() <= end) {
@@ -50,4 +71,6 @@ public class MarriedBouquet implements Bouquet<GeneralFlower> {
     public void setAssembledPrice(float price) {
         assemblePrice = price;
     }
+
+
 }
--- FlowerGarden/src/test/java/com/flowergarden/bouquet/MarriedBouquetTest.java
+++ FlowerGarden/src/test/java/com/flowergarden/bouquet/MarriedBouquetTest.java
@@ -1,36 +1,46 @@
 package com.flowergarden.bouquet;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import com.flowergarden.exception.IlligalParameterException;
 import com.flowergarden.flowers.Chamomile;
-import com.flowergarden.flowers.GeneralFlower;
 import com.flowergarden.flowers.Rose;
+import com.flowergarden.flowers.Tulip;
+import com.flowergarden.properties.FreshnessInteger;
 
 /**
  * Created by inna.pshenychna on 2/26/2018.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class MarriedBouquetTest {
 
     private MarriedBouquet marriedBouquet;
+    @Mock
     private Rose rose;
+    @Mock
+    private Chamomile chamomile;
 
     @Before
     public void init() {
-        Chamomile chamomile = mock(Chamomile.class);
         when(chamomile.getPrice()).thenReturn((float) 25.23);
         when(chamomile.getLenght()).thenReturn(20);
+        when(chamomile.getFreshness()).thenReturn(new FreshnessInteger(1));
 
-        rose = mock(Rose.class);
         when(rose.getPrice()).thenReturn((float) 30.30);
         when(rose.getLenght()).thenReturn(25);
+        when(rose.getFreshness()).thenReturn(new FreshnessInteger(1));
 
         marriedBouquet = new MarriedBouquet();
         marriedBouquet.addFlower(rose);
@@ -47,13 +57,60 @@ public class MarriedBouquetTest {
         Assert.assertEquals(Collections.singletonList(rose), marriedBouquet.searchFlowersByLenght(23, 27));
     }
 
-    @Test
+    @Test(expected = IlligalParameterException.class)
     public void searchFlowersByLenghtWhenMinGraterThanMaxTest() {
-        Assert.assertEquals(new ArrayList<GeneralFlower>(),marriedBouquet.searchFlowersByLenght(27, 20));
+        marriedBouquet.searchFlowersByLenght(27, 20);
+    }
+
+    @Test(expected = IlligalParameterException.class)
+    public void searchFlowersByLenghtWithNegativeMinMaxValuesTest() {
+        marriedBouquet.searchFlowersByLenght(-1, 4);
     }
 
     @Test
-    public void searchFlowersByLenghtWithNegativeMinMaxValuesTest() {
-        Assert.assertEquals(new ArrayList<GeneralFlower>(),marriedBouquet.searchFlowersByLenght(-10, -1));
+    public void flowerListTest() {
+        List list = mock(List.class);
+        MarriedBouquet marriedBouquet = new MarriedBouquet();
+        marriedBouquet.setFlowerListForMock(list);
+
+        marriedBouquet.addFlower(chamomile);
+
+        verify(list).add(chamomile);
     }
+
+
+    @Test(expected = IlligalParameterException.class)
+    public void addFlowerWithNegativePriceTest() {
+        Tulip tulip = mock(Tulip.class);
+        when(tulip.getPrice()).thenReturn((float) -1);
+
+        marriedBouquet.addFlower(tulip);
+    }
+
+    @Test(expected = IlligalParameterException.class)
+    public void addFlowerWithNegativeLengthTest() {
+        Tulip tulip = mock(Tulip.class);
+        when(tulip.getLenght()).thenReturn(-1);
+
+        marriedBouquet.addFlower(tulip);
+    }
+
+
+    @Test(expected = IlligalParameterException.class)
+    public void addFlowerWithNegativeFreshnessTest() {
+        Tulip tulip = mock(Tulip.class);
+        when(tulip.getFreshness()).thenReturn(new FreshnessInteger(-1));
+
+        marriedBouquet.addFlower(tulip);
+    }
+
+    @Test
+    public void setAssebledPriceTest(){
+        MarriedBouquet marriedBouquet = new MarriedBouquet();
+        marriedBouquet.setAssembledPrice(100);
+
+        Assert.assertEquals(100, marriedBouquet.getPrice(), 0.01);
+    }
+
+
 }
